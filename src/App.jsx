5e89b05027eac4346f70b5803df291960814f435
Viraj Tailor <virajtlr2006@gmail.com>
2025-07-30 23:44:11 +0530

    FeatureSection Completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import NavBar from './components/NavBar.jsx'
 import HeroSection from './components/HeroSection.jsx'
+import FeatureSection from './components/FeatureSection.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -11,6 +12,7 @@ function App() {
       <div className='max-w-7xl mx-auto pt-20 px-6'>
         <HeroSection/>
       </div>
+      <FeatureSection/>
     </div>
   ) 
 }
