8157d4c6a947a45e7de96a1e3addeda4c0f99aab
Viraj Tailor <virajtlr2006@gmail.com>
2025-07-30 23:16:59 +0530

    HeroSection Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import NavBar from './components/NavBar.jsx'
+import HeroSection from './components/HeroSection.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
       <NavBar/>
+      <div className='max-w-7xl mx-auto pt-20 px-6'>
+        <HeroSection/>
+      </div>
     </div>
   ) 
 }
